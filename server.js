ecfa1ca27de54a0f4ec280539921027d116f9918
Bajracharya <bajracj@38f9d3dac8ef.ant.amazon.com>
2019-12-06 16:39:09 -0800

    added port

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const express = require('express');
 const app = express();
 
 
-
-app.listen(3000, function() {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
     console.log('server started on port 3000');
 });
 
